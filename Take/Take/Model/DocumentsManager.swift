bdd8a2b8b8c6e9e4eb697c5a44b15f36c082a69f
Roopesh Chander <roop@roopc.net>
2014-07-18 13:22:54 +0530

    Add a DocumentsManager

---
+++ Take/Take/Model/DocumentsManager.swift
@@ -0,0 +1,40 @@
+//
+//  DocumentsManager.swift
+//  Take
+//
+//  Created by Roopesh Chander on 18/07/14.
+//  Copyright (c) 2014 Roopesh Chander. All rights reserved.
+//
+
+import UIKit
+
+class DocumentsManager {
+    var _iCloudManager: ICloudManager!
+    var _documentsRootURL: NSURL!
+
+    init() {
+        _iCloudManager = ICloudManager(delegate: self)
+        if (_iCloudManager.isLoggedIntoiCloud && _iCloudManager.ubiquityContainerURL) {
+            _documentsRootURL = NSURL(string: "Documents", relativeToURL: _iCloudManager.ubiquityContainerURL)
+        } else {
+            _documentsRootURL = NSFileManager.defaultManager().URLsForDirectory(
+                NSSearchPathDirectory.DocumentDirectory,
+                inDomains: NSSearchPathDomainMask.UserDomainMask)[0] as? NSURL
+        }
+    }
+}
+
+extension DocumentsManager: iCloudManagerDelegate {
+
+    func gotAccessToUbiquityContainer() {
+        assert(_iCloudManager.isLoggedIntoiCloud)
+        assert(_iCloudManager.ubiquityContainerURL)
+        _documentsRootURL = NSURL(string: "Documents", relativeToURL: _iCloudManager.ubiquityContainerURL)
+    }
+
+    // TODO
+    func loggedIntoiCloud() { NSLog("Logged into iCloud") }
+    func loggedOutOfiCloud() { NSLog("Logged out of iCloud") }
+    func iCloudUserChanged() { NSLog("iCloud user changed") }
+
+}
